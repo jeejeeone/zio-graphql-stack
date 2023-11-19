6855aa425908ff28b7d0d1a86d4384b9559b432e
Jari Jokisalo <jarijoki1@gmail.com>
2023-11-19 21:45:00 +0200

    tweak

--- src/main/scala/com/zap/graphql/GraphQLApi.scala
+++ src/main/scala/com/zap/graphql/GraphQLApi.scala
@@ -8,13 +8,13 @@ import com.zap.graphql.queries.PersonGraphQL
 import com.zap.model.{AddressId, PersonId}
 import zio.{ZIO, ZLayer}
 
-case class GraphQLApi(personQuery: PersonGraphQL):
+case class GraphQLApi(personGraphQL: PersonGraphQL):
   import caliban.schema.Schema.auto.*
 
   given Schema[Any, PersonId]  = Schema.intSchema.contramap(v => PersonId.unwrap(v))
   given Schema[Any, AddressId] = Schema.intSchema.contramap(v => AddressId.unwrap(v))
 
-  val api = graphQL(RootResolver(Queries(personQuery.personsQuery())))
+  val api = graphQL(RootResolver(Queries(personGraphQL.personsQuery())))
 
 object GraphQLApi:
   val live = ZLayer:
